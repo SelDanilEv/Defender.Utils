6e2122771deb12a5feee389e5bbdf9d46f2f6086
Danil <47692381+SelDanilEv@users.noreply.github.com>
2023-04-22 15:02:19 +0400

    Update EncryptionUtils.cs

--- src/Defender.Utils/EncryptionUtils.cs
+++ src/Defender.Utils/EncryptionUtils.cs
@@ -6,9 +6,21 @@ namespace Defender.Utils
 {
     public class EncryptionUtils
     {
-        public string GetHashSHA1(string text, string salt = "")
+        public static string GetHashSHA1(string text, string salt = "")
         {
-            using (SHA1Managed sha1 = new SHA1Managed())
+            using (var sha1 = new SHA1Managed())
+            {
+                byte[] hash = Encoding.UTF8.GetBytes($"{salt}_{text}");
+                byte[] generatedHash = sha1.ComputeHash(hash);
+                string generatedHashString = Convert.ToBase64String(generatedHash);
+
+                return generatedHashString.Trim('=');
+            }
+        }
+
+        public static string GetHashSHA256(string text, string salt = "")
+        {
+            using (var sha1 = new SHA256Managed())
             {
                 byte[] hash = Encoding.UTF8.GetBytes($"{salt}_{text}");
                 byte[] generatedHash = sha1.ComputeHash(hash);
